1471479d44d0dd18458654fefd0f79d93b5fa49c
Bassilio <bassilede88@gmail.com>
2024-07-02 17:53:43 +0200

    loading css and js files

---
+++ index.js
@@ -0,0 +1,47 @@
+
+const resultText = document.getElementById("resultText");
+const playerText = document.getElementById("playerText");
+const computerText = document.getElementById("computerText");
+
+const choiceButtons = document.querySelectorAll(".choiceBtn");
+
+let playerChoice;
+let computerChoice;
+let gameResult;
+
+choiceButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    playerChoice = button.textContent;
+    computerTurn();
+    playerText.textContent = `Me: ${playerChoice}`;
+    computerText.textContent = `Computer: ${computerChoice}`;
+    resultText.textContent = checkWinner();
+  })
+);
+
+function checkWinner() {
+  if (playerChoice === computerChoice) {
+    return "Draw!";
+  } else if (computerChoice === "✊") {
+    return playerChoice === "✋" ? "You Win! 😊" : "You Lose! 😢";
+  } else if (computerChoice === "✋") {
+    return playerChoice === "✌️" ? "You Win! 😊" : "You Lose! 😢";
+  } else if (computerChoice === "✌️") {
+    return playerChoice === "✊" ? "You Win! 😊" : "You Lose! 😢";
+  }
+}
+
+function computerTurn() {
+  const randomNum = Math.floor(Math.random() * 3);
+  switch (randomNum) {
+    case 0:
+      computerChoice = "✊";
+      break;
+    case 1:
+      computerChoice = "✋";
+      break;
+    case 2:
+      computerChoice = "✌️";
+      break;
+  }
+}
